d54fd260511cde34b6c9cc098ff750e264f566b8
Maurice Kratz <kratz.maurice@gmail.com>
2022-09-06 20:48:20 -0500

    list visibility

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ window.addEventListener('load', () => {
 		let fuelLevel = document.querySelector('input[name=fuelLevel]').value;
 		let cargoLevel = document.querySelector('input[name=cargoMass]').value;
 		let list = document.getElementById('faultyItems');
+		list.style.visibility = 'hidden';
 
 		// Validate user input with formSubmission function from scriptHelper.js
 		formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
