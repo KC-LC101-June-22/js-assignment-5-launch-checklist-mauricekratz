a3c21d3306c8f7296d7b4224aadd2031d2874dc2
Maurice Kratz <kratz.maurice@gmail.com>
2022-09-06 21:36:52 -0500

    trying to fix fetch

--- scriptHelper.js
+++ scriptHelper.js
@@ -101,14 +101,10 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 }
 
 async function myFetch() {
-	let planetsReturned;
-
-	planetsReturned = await fetch(
-		'https://handlers.education.launchcode.org/static/planets.json'
-	).then(function (response) {
-		return response.json();
-	});
-	return planetsReturned;
+    let planetsReturned = await fetch('https://handlers.education.launchcode.org/static/planets.json').then( function(response) {
+        return response.json();
+        });
+    return planetsReturned;
 }
 
 function pickPlanet(planets) {
